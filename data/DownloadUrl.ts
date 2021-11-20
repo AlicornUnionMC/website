faf4b959874b6352a684946e4e3a7d8f2df5323d
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-11-20 21:39:48 +0800

    update url

--- data/DownloadUrl.ts
+++ data/DownloadUrl.ts
@@ -1,7 +1,7 @@
 const DOWNLOAD_WIN32 =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/Md2pDGcv/wGCv-ZAU74v8ejIQq8vRD_dF3avQQMpNnk1fRHERnBg*";
+  "https://pan.bilnn.com/api/v3/file/sourcejump/Oq9Z96HO/V0uBCc5zutPpW6msTjkQ_SlAetkUneDCX9WpUTgOTe0*";
 const DOWNLOAD_GNU =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/eWX9vzTZ/kPOemhNm-jwWMgtu1YYldrGNWm2WijRoTGpisok_0ws*";
+  "https://pan.bilnn.com/api/v3/file/sourcejump/zZ8A8DSq/r9WYbsctSoxNmi9iQ07BLq9jJ7G-9z-KwFPJnvO0zHk*";
 
 export const getUrl = () => {
   if (navigator.userAgent.toLowerCase().includes("win")) {
