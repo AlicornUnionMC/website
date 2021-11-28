4c25a02a0730b1e8a891a66c14fc93961823c6dd
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-11-28 21:59:03 +0800

    update url

--- data/DownloadUrl.ts
+++ data/DownloadUrl.ts
@@ -1,7 +1,7 @@
 const DOWNLOAD_WIN32 =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/Z9BNepSo/6dJ0nw01gjfrHZhQfkrAkL_N6QXI6SxSGB3zoG4rb7c*";
+  "https://hub.fastgit.org/Andy-K-Sparklight/AlicornBinaries/releases/download/latest/Alicorn-win32-x64.zip";
 const DOWNLOAD_GNU =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/3nGPpGhD/eWsS5UYI-bUgu6lvnJWryIW2bwCAzS4nDOg3ygfxVqQ*";
+  "https://hub.fastgit.org/Andy-K-Sparklight/AlicornBinaries/releases/download/latest/Alicorn-linux-x64.tar.gz";
 
 export const getUrl = () => {
   if (navigator.userAgent.toLowerCase().includes("win")) {
