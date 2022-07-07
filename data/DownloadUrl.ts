9a52d7400fc25071bb30dc9fa58b80d57d9222d3
Annie K Rarity Sparklight <Twilight_Princess_Sparkle@outlook.com>
2022-07-07 18:24:28 +0800

    Update DownloadUrl.ts

--- data/DownloadUrl.ts
+++ data/DownloadUrl.ts
@@ -1,7 +1,7 @@
 const DOWNLOAD_WIN32 =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/6WrxyMSg/ZDxFAo_87LLl2TCH2866jhCaeqz0iC2YTWmQnoSU6rs*";
+  "https://pan.bilnn.com/api/v3/file/sourcejump/J8D1N1Fz/7pyVSQiKbkqij1zjPCA1wQ_B5wD5RlntFVgDZCYRTJs*";
 const DOWNLOAD_GNU =
-  "https://pan.bilnn.com/api/v3/file/sourcejump/pm5P9mcw/PFwf-mI9lHcX4np8wipsTbcZiZz054kE5rKQ-P8QlO0*";
+  "https://pan.bilnn.com/api/v3/file/sourcejump/E5YQXbc9/qb3x77XW1_7hitnNK-JySdK3V7Xv92Y2u1GMgWkbNsY*";
 
 export const getUrl = () => {
   if (navigator.userAgent.toLowerCase().includes("win")) {
